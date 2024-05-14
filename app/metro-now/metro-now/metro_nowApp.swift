8345bd6ba6405e30d46ef4a21f3f26229e6d5f1f
Krystof <krystof.kratky2003@gmail.com>
2024-05-14 18:47:43 +0200

    feat(app): init

---
+++ app/metro-now/metro-now/metro_nowApp.swift
@@ -0,0 +1,17 @@
+//
+//  metro_nowApp.swift
+//  metro-now
+//
+//  Created by Kryštof Krátký on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct metro_nowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
