2d76648f232e716432c3da54db31f0c3422cc7c0
Krystof <krystof.kratky2003@gmail.com>
2024-05-19 23:17:25 +0200

    feat(app): watch app target

---
+++ app/metro-now/metro-now-watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  metro-now-watch Watch App
+//
+//  Created by Kryštof Krátký on 19.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/metro-now/metro-now-watch Watch App/metro_now_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  metro_now_watchApp.swift
+//  metro-now-watch Watch App
+//
+//  Created by Kryštof Krátký on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct metro_now_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
