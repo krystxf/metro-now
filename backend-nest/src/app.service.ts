586f298da6850760c174be0bd960e1e3776b8fe4
Krystof <krystof.kratky2003@gmail.com>
2024-06-18 07:39:47 +0200

    feat(docs): remove hello world

--- backend-nest/src/app.service.ts
+++ backend-nest/src/app.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello World!";
+        return `
+            <h1>
+                Hello, fellow developer👋
+            </h1>
+            <p>
+                Thank you for checking out my project.
+                You can find the API documentation <a href="https://metro-now.vercel.app/docs/rest-api">here</a>.
+            </p>
+        `;
     }
 }
