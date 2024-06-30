0dd946df18ce7644207ac20f56ae9ab44cff673a
Krystof <krystof.kratky2003@gmail.com>
2024-06-30 12:34:12 +0200

    feat: logging

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MetroController } from "./metro/metro.controller";
 import { ConfigModule } from "@nestjs/config";
+import { RequestLoggerMiddleware } from "./middleware/request-logger.middleware";
 @Module({
     imports: [ConfigModule.forRoot()],
     controllers: [AppController, MetroController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(RequestLoggerMiddleware).forRoutes("*");
+    }
+}
