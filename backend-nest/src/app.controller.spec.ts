2dd1b5167c3795e0724274c4b182b511c86d0acc
Krystof <krystof.kratky2003@gmail.com>
2024-06-30 11:54:58 +0200

    refactor: move test files

--- backend-nest/src/app.controller.spec.ts
+++ backend-nest/src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AppService, WELCOME_MESSAGE } from "./app.service";
 
 describe("AppController", () => {
     let appController: AppController;
@@ -15,8 +15,8 @@ describe("AppController", () => {
     });
 
     describe("root", () => {
-        it('should return "Hello World!"', () => {
-            expect(appController.getHello()).toBe("Hello World!");
+        it("should return welcome message", () => {
+            expect(appController.getHello()).toBe(WELCOME_MESSAGE);
         });
     });
 });
