05a2d7c030f3bb80a36da43a8aa564bc7ec9c4e2
Krystof <krystof.kratky2003@gmail.com>
2024-07-04 22:41:56 +0200

    refactor(ios): use native countdown

--- app/metro-now/Core/PlatformList/PlatformListItem.swift
+++ app/metro-now/Core/PlatformList/PlatformListItem.swift
@@ -13,13 +13,7 @@ struct PlatformListItemView: View {
     /// this view doesn't handle logic of deciding which departures are outdated (shouldn't be shown)
     @State var departureDates: [Date]
 
-    /// time until departure in human readable form
-    @State private var departureStrings: [String] = []
-
     @State var metroLine: String
-    private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
-
-    @State private var deltatime: Double = 0
 
     var body: some View {
         HStack {
@@ -34,35 +28,29 @@ struct PlatformListItemView: View {
             Spacer()
 
             VStack {
-                if departureStrings.count >= 1 {
-                    Text(departureStrings[0])
+                if departureDates.count >= 1 {
+                    Text(
+                        .currentDate, format: .reference(to: departureDates[0], allowedFields: [.second, .minute, .hour])
+                    )
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .foregroundStyle(.white)
+                }
+                if departureDates.count >= 2 {
+                    Text(
+                        "Also in "
+                    ).font(.caption2)
                         .fontWeight(.bold)
                         .foregroundStyle(.white)
-                        .foregroundStyle(.white)
-                }
-                if departureStrings.count >= 2 {
+                        .opacity(0.9)
                     Text(
-                        "Also in \(departureStrings[1])"
+                        .currentDate, format: .reference(to: departureDates[1], allowedFields: [.second, .minute, .hour])
                     )
                     .font(.caption2)
                     .fontWeight(.bold)
                     .foregroundStyle(.white)
                     .opacity(0.9)
                 }
-            }
-        }
-        .onReceive(timer) {
-            _ in
-            guard departureDates.count > 0 else { return }
-
-            if departureDates.count >= 2 {
-                departureStrings = departureDates[0 ..< 2].map {
-                    formatTime(seconds: secondsFromNow($0))
-                }
-            } else {
-                departureStrings = [
-                    formatTime(seconds: secondsFromNow(departureDates[0])),
-                ]
             }
         }
         .padding(.horizontal, 20)
