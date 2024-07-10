4e9d5d459d690523479a553722d3c2224b4b2ef8
Krystof <krystof.kratky2003@gmail.com>
2024-07-10 03:41:18 +0200

    fix: docker compose files

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -4,20 +4,25 @@ import { WinstonModule } from "nest-winston";
 import { format, transports } from "winston";
 import "winston-daily-rotate-file";
 
+const config =
+    process.env.NODE_ENV === "production"
+        ? {
+              logger: WinstonModule.createLogger({
+                  format: format.json({}),
+                  transports: ["info", "http", "warn", "error"].map((level) => {
+                      return new transports.DailyRotateFile({
+                          handleExceptions: level === "error",
+                          filename: `logs/%DATE%-${level}.log`,
+                          level,
+                          format: format.combine(format.timestamp()),
+                      });
+                  }),
+              }),
+          }
+        : {};
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        logger: WinstonModule.createLogger({
-            format: format.json({}),
-            transports: ["info", "http", "warn", "error"].map((level) => {
-                return new transports.DailyRotateFile({
-                    handleExceptions: level === "error",
-                    filename: `logs/%DATE%-${level}.log`,
-                    level,
-                    format: format.combine(format.timestamp()),
-                });
-            }),
-        }),
-    });
+    const app = await NestFactory.create(AppModule, config);
     await app.listen(3001);
 }
 bootstrap();
