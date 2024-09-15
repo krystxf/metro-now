09b9758d9d29aa00c429b494780c56f59191ebc2
Krystof <krystof.kratky2003@gmail.com>
2024-09-15 19:20:36 +0200

    refactor: remove logger

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,10 +1,10 @@
-import { MiddlewareConsumer, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { CacheInterceptor, CacheModule } from "@nestjs/cache-manager";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MetroController } from "./controllers/metro/metro.controller";
 import { ConfigModule } from "@nestjs/config";
-import { RequestLoggerMiddleware } from "./middleware/request-logger.middleware";
+
 import { TTL_DEFAULT } from "./constants/constants";
 import { ScheduleModule } from "@nestjs/schedule";
 import { SyncStopsService } from "./services/sync-stops.service";
@@ -32,8 +32,4 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
         },
     ],
 })
-export class AppModule {
-    configure(consumer: MiddlewareConsumer) {
-        consumer.apply(RequestLoggerMiddleware).forRoutes("*");
-    }
-}
+export class AppModule {}
