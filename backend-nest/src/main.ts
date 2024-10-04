3f23adfc74136d3e73578fa13e3f007e9bb2b8c4
Krystof <krystof.kratky2003@gmail.com>
2024-10-04 20:32:14 +0200

    feat(be): swagger documentation

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import {
+    SWAGGER_DESCRIPTION,
+    SWAGGER_JSON_URL,
+    SWAGGER_TITLE,
+    SWAGGER_API_ROOT,
+    SWAGGER_VERSION,
+} from "./swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle(SWAGGER_TITLE)
+        .setDescription(SWAGGER_DESCRIPTION)
+        .setVersion(SWAGGER_VERSION)
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(SWAGGER_API_ROOT, app, document, {
+        jsonDocumentUrl: SWAGGER_JSON_URL,
+        customSiteTitle: SWAGGER_TITLE,
+    });
+
     await app.listen(3001);
 }
 bootstrap();
