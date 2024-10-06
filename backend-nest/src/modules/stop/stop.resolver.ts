35150f887febf005f6a97ae35da1efd256ca701c
Krystof <krystof.kratky2003@gmail.com>
2024-10-06 15:02:41 +0200

    feat(be): graphql resolvers

---
+++ backend-nest/src/modules/stop/stop.resolver.ts
@@ -0,0 +1,25 @@
+import { Args, Query, Resolver } from "@nestjs/graphql";
+
+import { Stop } from "src/models/stop.model";
+import { StopService } from "src/modules/stop/stop.service";
+import { metroOnlyQuery } from "src/swagger/query.swagger";
+
+@Resolver(() => Stop)
+export class StopResolver {
+    constructor(private readonly stopSerice: StopService) {}
+
+    @Query(() => [Stop], {
+        description: "Get all stops",
+    })
+    async stop(
+        @Args("metroOnly", {
+            description: metroOnlyQuery.description,
+            defaultValue: false,
+            nullable: true,
+            type: () => Boolean,
+        })
+        metroOnly,
+    ): Promise<Stop[]> {
+        return await this.stopSerice.getAll({ metroOnly });
+    }
+}
