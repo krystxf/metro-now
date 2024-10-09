b92e182024db324d9c5599388854e67db83914a6
Krystof <krystof.kratky2003@gmail.com>
2024-10-09 10:36:18 +0200

    chore(be): e2e tests

---
+++ apps/backend/e2e/tests/departure.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { CacheModule } from "@nestjs/cache-manager";
+import { INestApplication } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { Test, TestingModule } from "@nestjs/testing";
+import * as request from "supertest";
+
+import { cacheModuleConfig } from "src/config/cache-module.config";
+import { configModuleConfig } from "src/config/config-module.config";
+import { DepartureModule } from "src/modules/departure/departure.module";
+import { PrismaModule } from "src/modules/prisma/prisma.module";
+
+describe("Departure Module (e2e)", () => {
+    let app: INestApplication;
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [
+                CacheModule.register(cacheModuleConfig),
+                ConfigModule.forRoot(configModuleConfig),
+                PrismaModule,
+                DepartureModule,
+            ],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it.each([`U1000Z1`, `U1000Z10`])(
+        "[GET] /departure/platform?id=%s",
+        (platform) => {
+            return request(app.getHttpServer())
+                .get(`/departure/platform?id=${platform}`)
+                .expect(200)
+                .accept("application/json");
+        },
+    );
+});
