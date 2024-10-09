5e9d9e2fd64a5cd3d83754023440e68bc8acd657
Krystof <krystof.kratky2003@gmail.com>
2024-10-09 10:35:33 +0200

    refactor(be): extract configs from app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -10,32 +10,26 @@ import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ScheduleModule } from "@nestjs/schedule";
 
+import { cacheModuleConfig } from "src/config/cache-module.config";
+import { configModuleConfig } from "src/config/config-module.config";
 import { GRAPHQL_API_ROOT } from "src/constants/graphql.const";
-import { Environment } from "src/enums/environment.enum";
 import { LoggerMiddleware } from "src/middleware/logger.middleware";
 import { DepartureModule } from "src/modules/departure/departure.module";
 import { ImportModule } from "src/modules/import/import.module";
 import { PlatformModule } from "src/modules/platform/platform.module";
 import { PrismaModule } from "src/modules/prisma/prisma.module";
 import { StopModule } from "src/modules/stop/stop.module";
-import { envSchema } from "src/schema/env.schema";
 
 @Module({
     imports: [
         PlatformModule,
         DepartureModule,
-        ...(process.env.NODE_ENV === Environment.TEST ? [] : [ImportModule]),
+        ImportModule,
         StopModule,
-
         PrismaModule,
-        ConfigModule.forRoot({
-            envFilePath: ".env.local",
-            validate: envSchema.parse,
-        }),
+        ConfigModule.forRoot(configModuleConfig),
         ScheduleModule.forRoot(),
-        CacheModule.register({
-            isGlobal: true,
-        }),
+        CacheModule.register(cacheModuleConfig),
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             playground: true,
