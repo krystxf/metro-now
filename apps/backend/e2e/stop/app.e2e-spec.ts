26321cdd15b4c2f83a62bd37539b3d5dda0fb49d
Krystof <krystof.kratky2003@gmail.com>
2024-10-09 09:10:54 +0200

    chore(be): e2e tests

---
+++ apps/backend/e2e/stop/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { INestApplication } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import * as request from "supertest";
+
+import { AppModule } from "src/app.module";
+
+describe("AppController (e2e)", () => {
+    let app: INestApplication;
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it("/stop/all (GET)", () => {
+        return request(app.getHttpServer())
+            .get("/stop/all")
+            .expect(200)
+            .accept("application/json");
+    });
+});
