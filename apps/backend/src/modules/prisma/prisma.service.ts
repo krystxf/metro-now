cd23cabb07dca23d394747d473a1e5f798eca911
Krystof <krystof.kratky2003@gmail.com>
2024-10-17 23:03:01 +0200

    fix(be): disconnect prisma

--- apps/backend/src/modules/prisma/prisma.service.ts
+++ apps/backend/src/modules/prisma/prisma.service.ts
@@ -1,8 +1,11 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+{
     constructor() {
         super({
             datasourceUrl: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.POSTGRES_DB}?schema=${process.env.DB_SCHEMA}`,
@@ -10,5 +13,9 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     }
     async onModuleInit() {
         await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
     }
 }
