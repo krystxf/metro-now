fdf02300bb349306f24739973205f0c90b84337a
Krystof <krystof.kratky2003@gmail.com>
2024-10-26 14:28:30 +0200

    feat(be): extend departure endpoint

--- apps/backend/src/modules/departure/departure.controller.ts
+++ apps/backend/src/modules/departure/departure.controller.ts
@@ -10,12 +10,15 @@ import { ApiTags } from "@nestjs/swagger";
 import { z } from "zod";
 
 import { QUERY_IDS_COUNT_MAX } from "src/constants/constants";
+import { ApiQueries } from "src/decorators/swagger.decorator";
 import { DepartureService } from "src/modules/departure/departure.service";
 import {
     departureSchema,
     type DepartureSchema,
 } from "src/modules/departure/schema/departure.schema";
 import { LogInterceptor } from "src/modules/logger/log.interceptor";
+import { metroOnlySchema } from "src/schema/metro-only.schema";
+import { metroOnlyQuery } from "src/swagger/query.swagger";
 import { toArray } from "src/utils/array.utils";
 
 @ApiTags("departure")
@@ -23,6 +26,57 @@ import { toArray } from "src/utils/array.utils";
 @UseInterceptors(LogInterceptor)
 export class DepartureController {
     constructor(private readonly departureService: DepartureService) {}
+
+    @Get()
+    @ApiQueries([
+        metroOnlyQuery,
+        {
+            name: "platform[]",
+            description: "Platform IDs",
+            type: String,
+            isArray: true,
+            allowEmptyValue: true,
+            required: false,
+        },
+        {
+            name: "stop[]",
+            description: "Stop IDs",
+            type: String,
+            isArray: true,
+            allowEmptyValue: true,
+            required: false,
+        },
+    ])
+    async getDepartures(@Query() query): Promise<DepartureSchema[]> {
+        const schema = z.object({
+            metroOnly: metroOnlySchema,
+            platform: z.string().array().optional().default([]),
+            stop: z.string().array().optional().default([]),
+        });
+        const parsed = schema.safeParse(query);
+        if (!parsed.success) {
+            throw new HttpException(
+                "Invalid query params",
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+        const parsedQuery = parsed.data;
+
+        if (parsedQuery.platform.length + parsedQuery.stop.length === 0) {
+            throw new HttpException(
+                "At least one platform or stop ID must be provided",
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+
+        const departures = await this.departureService.getDepartures({
+            stopIds: parsedQuery.stop,
+            platformIds: parsedQuery.platform,
+            metroOnly: parsedQuery.metroOnly,
+        });
+
+        return departureSchema.array().parse(departures);
+    }
 
     @Get("/platform")
     async getDeparturesByPlatform(@Query("id") id): Promise<DepartureSchema[]> {
@@ -40,9 +94,11 @@ export class DepartureController {
             );
         }
 
-        const departures = await this.departureService.getDeparturesByPlatform(
-            parsed.data,
-        );
+        const departures = await this.departureService.getDepartures({
+            stopIds: [],
+            platformIds: parsed.data,
+            metroOnly: false,
+        });
 
         return departureSchema.array().parse(departures);
     }
--- apps/backend/src/modules/departure/departure.service.ts
+++ apps/backend/src/modules/departure/departure.service.ts
@@ -1,4 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { unique } from "radash";
 
 import { GOLEMIO_API } from "src/constants/golemio.const";
 import { departureBoardsSchema } from "src/modules/departure/schema/departure-boards.schema";
@@ -9,6 +10,84 @@ import { getDelayInSeconds } from "src/utils/delay";
 @Injectable()
 export class DepartureService {
     constructor(private prisma: PrismaService) {}
+
+    async getDepartures(args: {
+        stopIds: string[];
+        platformIds: string[];
+        metroOnly: boolean;
+    }): Promise<DepartureSchema[]> {
+        const dbPlatforms = (
+            await this.prisma.platform.findMany({
+                select: { id: true },
+                where: {
+                    id: { in: args.platformIds },
+                    ...(args.metroOnly ? { isMetro: true } : {}),
+                },
+            })
+        ).map((platform) => platform.id);
+        const stopPlatforms = (
+            await this.prisma.stop.findMany({
+                select: {
+                    platforms: {
+                        select: { id: true },
+                        where: { ...(args.metroOnly ? { isMetro: true } : {}) },
+                    },
+                },
+                where: { id: { in: args.stopIds } },
+            })
+        ).flatMap((stop) => stop.platforms.map((platform) => platform.id));
+        const allPlatformIds = unique([...dbPlatforms, ...stopPlatforms]).slice(
+            0,
+            100,
+        );
+
+        const searchParams = new URLSearchParams(
+            allPlatformIds
+                .map((id) => ["ids", id])
+                .concat([
+                    ["skip", "canceled"],
+                    ["mode", "departures"],
+                    ["order", "real"],
+                ]),
+        );
+
+        const url = new URL(
+            `${GOLEMIO_API}/v2/pid/departureboards?${searchParams.toString()}`,
+        );
+
+        const res = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Access-Token": process.env.GOLEMIO_API_KEY ?? "",
+            },
+        });
+        if (!res.ok) {
+            throw new Error(
+                `Failed to fetch departure data: ${res.status} ${res.statusText}`,
+            );
+        }
+
+        const json = await res.json();
+        const parsed = departureBoardsSchema.safeParse(json);
+
+        if (!parsed.success) {
+            throw new Error(parsed.error.message);
+        }
+
+        const parsedDepartures = parsed.data.departures.map((departure) => {
+            return {
+                departure: departure.departure_timestamp,
+                delay: getDelayInSeconds(departure.delay),
+                headsign: departure.trip.headsign,
+                route: departure.route.short_name,
+                platformId: departure.stop.id,
+                platformCode: departure.stop.platform_code,
+            };
+        });
+
+        return parsedDepartures;
+    }
 
     async getDeparturesByPlatform(
         requestedIds: string[],
