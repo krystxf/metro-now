8f070bbd530301b8d3d4b4389c32751cdf77bbaa
Krystof <krystof.kratky2003@gmail.com>
2024-10-28 02:58:26 +0100

    fix(be): timestamp functionality

--- apps/backend/src/schema/timestamp.schema.ts
+++ apps/backend/src/schema/timestamp.schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const timestampSchema = z.object({
-    predicted: z.coerce.date(),
-    scheduled: z.coerce.date(),
+    predicted: z.string(),
+    scheduled: z.string(),
 });
 
 export type TimestampSchema = z.infer<typeof timestampSchema>;
