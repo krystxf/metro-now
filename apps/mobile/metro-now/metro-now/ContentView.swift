e2104457a9b920ffa7eff03faa544330efee37a3
Krystof <krystof.kratky2003@gmail.com>
2024-11-21 21:42:35 +0100

    feat(app): welcome screen

--- apps/mobile/metro-now/metro-now/ContentView.swift
+++ apps/mobile/metro-now/metro-now/ContentView.swift
@@ -4,9 +4,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showWelcomeScreen = true
+
     var body: some View {
         NavigationStack {
             ClosestStopPageView()
+
+        }.sheet(
+            isPresented: $showWelcomeScreen,
+            onDismiss: {
+                showWelcomeScreen = false
+            }
+        ) {
+            WelcomeScreenView(
+                handleSubmit: {
+                    showWelcomeScreen = false
+                }
+            )
+            .presentationDetents([.medium])
         }
     }
 }
