235a2f20d0c1d3932e07f93404fc8f543b8bd2fb
Krystof <krystof.kratky2003@gmail.com>
2024-11-21 22:48:10 +0100

    feat(app): show no network banner

--- apps/mobile/metro-now/metro-now/ContentView.swift
+++ apps/mobile/metro-now/metro-now/ContentView.swift
@@ -4,24 +4,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showWelcomeScreen: Bool = false
+    @StateObject private var networkMonitor = NetworkMonitor()
+    @State private var showNoInternetBanner = false
+
     @AppStorage(
         AppStorageKeys.hasSeenWelcomeScreen.rawValue
     ) var hasSeenWelcomeScreen = false
+    @State private var showWelcomeScreen: Bool = false
 
     var body: some View {
-        NavigationStack {
-            ClosestStopPageView()
+        ZStack {
+            NavigationStack {
+                ClosestStopPageView()
+            }
+            .sheet(
+                isPresented: $showWelcomeScreen,
+                onDismiss: dismissWelcomeScreen
+            ) {
+                WelcomeScreenView(handleDismiss: dismissWelcomeScreen)
+                    .presentationDetents([.medium])
+            }
+
+            VStack {
+                Spacer()
+                if showNoInternetBanner {
+                    NoInternetBannerView()
+                        .transition(getBannerTransition(.bottom))
+                }
+            }
         }
         .onAppear {
             showWelcomeScreen = !hasSeenWelcomeScreen
         }
-        .sheet(
-            isPresented: $showWelcomeScreen,
-            onDismiss: dismissWelcomeScreen
-        ) {
-            WelcomeScreenView(handleDismiss: dismissWelcomeScreen)
-                .presentationDetents([.medium])
+        .onReceive(networkMonitor.$isConnected) { isConnected in
+            if !isConnected {
+                withAnimation {
+                    showNoInternetBanner = true
+                }
+
+                // Hide the banner after a delay
+                DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+                    withAnimation {
+                        showNoInternetBanner = false
+                    }
+                }
+            }
         }
     }
 
