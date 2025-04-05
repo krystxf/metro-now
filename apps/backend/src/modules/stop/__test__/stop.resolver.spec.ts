b753fd11bab20875407c022e19b610caac32a21d
Krystof <krystof.kratky2003@gmail.com>
2025-04-05 18:09:41 +0200

    feat(be): stop resolver test

---
+++ apps/backend/src/modules/stop/__test__/stop.resolver.spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from "@nestjs/testing";
+
+import { PrismaService } from "src/modules/prisma/prisma.service";
+import { StopResolver } from "src/modules/stop/stop.resolver";
+import { StopService } from "src/modules/stop/stop.service";
+
+describe("StopResolver", () => {
+    let resolver: StopResolver;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                StopResolver,
+                StopService,
+                {
+                    provide: PrismaService,
+                    useValue: {
+                        stop: {
+                            findMany: jest.fn(),
+                            findUnique: jest.fn(),
+                            findFirst: jest.fn(),
+                        },
+                        platform: {
+                            findMany: jest.fn(),
+                        },
+                        $transaction: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
+
+        resolver = module.get<StopResolver>(StopResolver);
+    });
+
+    it("should be defined", () => {
+        expect(resolver).toBeDefined();
+    });
+});
