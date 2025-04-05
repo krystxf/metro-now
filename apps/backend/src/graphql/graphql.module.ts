3a114355fcd9e6031550edd685ab2d734662a14a
Krystof <krystof.kratky2003@gmail.com>
2025-04-05 17:04:13 +0200

    refactor: gql schema

--- apps/backend/src/graphql/graphql.module.ts
+++ apps/backend/src/graphql/graphql.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 
 import { GraphqlResolver } from "src/graphql/graphql.resolver";
+import { GraphqlService } from "src/graphql/graphql.service";
 
 @Module({
-    providers: [GraphqlResolver],
+    providers: [GraphqlResolver, GraphqlService],
 })
 export class GraphqlModule {}
--- apps/backend/src/graphql/graphql.resolver.ts
+++ apps/backend/src/graphql/graphql.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from "@nestjs/graphql";
 
 import { IQuery } from "src/types/graphql.generated";
 
@@ -7,5 +7,16 @@ export class GraphqlResolver {
     @Query("stops")
     stops(): IQuery["stops"] {
         return [];
+    }
+
+    @Query("stop")
+    stop(@Args("id") id: string): IQuery["stop"] {
+        return {
+            id,
+            name: "Stop 1",
+            avgLatitude: 1,
+            avgLongitude: 1,
+            platforms: [],
+        };
     }
 }
