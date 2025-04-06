1378766d7f8af8f74ac4b3ac2df86a8d2c7a6623
Krystof <krystof.kratky2003@gmail.com>
2025-04-06 13:13:20 +0200

    fix(be): infotexts relatedPlatforms

--- apps/backend/src/modules/infotexts/infotexts.resolver.ts
+++ apps/backend/src/modules/infotexts/infotexts.resolver.ts
@@ -1,18 +1,35 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
 
 import { InfotextsService } from "src/modules/infotexts/infotexts.service";
-import { InfotextPriority, IQuery } from "src/types/graphql.generated";
+import { PrismaService } from "src/modules/prisma/prisma.service";
+import {
+    Infotext,
+    InfotextPriority,
+    IQuery,
+} from "src/types/graphql.generated";
 
-@Resolver()
+@Resolver("Infotext")
 export class InfotextsResolver {
-    constructor(private readonly infotextsService: InfotextsService) {}
+    constructor(
+        private readonly infotextsService: InfotextsService,
+        private prisma: PrismaService,
+    ) {}
 
     @Query("infotexts")
     async infotexts(): Promise<IQuery["infotexts"]> {
         return (await this.infotextsService.getAll()).map((infotext) => ({
             ...infotext,
             priority: infotext.priority as InfotextPriority,
-            relatedStops: infotext.relatedStops as any[],
+            relatedPlatforms: infotext.relatedStops as any[],
         }));
+    }
+
+    @ResolveField("relatedPlatforms")
+    getRelatedPlatforms(@Parent() infotext: Infotext) {
+        const ids = infotext.relatedPlatforms.map((p) => p.id);
+
+        return this.prisma.platform.findMany({
+            where: { id: { in: ids } },
+        });
     }
 }
