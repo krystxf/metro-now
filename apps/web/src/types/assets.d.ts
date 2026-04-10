b034085d75b9cbc7c812279d0199cd6688f306e6
Krystof <krystof.kratky2003@gmail.com>
2026-04-10 18:29:38 +0200

    fix(web): ci

--- apps/web/src/types/assets.d.ts
+++ apps/web/src/types/assets.d.ts
@@ -4,6 +4,6 @@ declare module "*.png" {
 }
 
 declare module "*.svg" {
-    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+    const content: string;
     export default content;
 }
